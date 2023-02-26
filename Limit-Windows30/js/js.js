781f90119196ce34b7d3cb4d5db2bd692db80d0c
Limit-Robot <113891689+Limit-Robot@users.noreply.github.com>
2023-02-26 18:18:00 +0800

    Add files via upload

--- Limit-Windows30/js/js.js
+++ Limit-Windows30/js/js.js
@@ -79,8 +79,11 @@ function win_login_2() {
 
 function win_login_3() {
     var f = document.getElementById("TaskBack");
-    f.style = "display: block;"
+    f.style = "display: block;";
+    var desk = document.getElementById("desktop");
+    desk.style = "display: block;";
 }
+
 function start() {
 	setTimeout(print1, 1400);
 	setTimeout(print2, 3600);
@@ -88,6 +91,7 @@ function start() {
 	setTimeout(zzh2, 4800);
 	setTimeout(zzh3, 10000)
 }
+
 function Start() {
 	setTimeout(print1, 10);
 	setTimeout(print2, 10);
@@ -97,18 +101,82 @@ function Start() {
 	setTimeout(win_login, 10);
 	setTimeout(win_login_2, 10);
 	setTimeout(win_login_3, 10);
+    // setTimeout(icon_blue_1, 10);
 }
-
-Start();
 
 function icon_blue_1() {
     ++tot1;
     var b = document.getElementById("Icon-1");
+    var dock = document.getElementsByTagName("div")[6];
     if (tot1 == 1) {
         b.style = "background-color: #ffffffc2;";
+        dock.style.animationName = "dock-up";
+        dock.style.animationDuration = "0.8s";
     }
     else if (tot1 >= 2) {
         b.style = "background-color: #ffffff00;";
+        dock.style.animationName = "dock-down";
+        dock.style.animationDuration = "0.8s";
+    }
+    setTimeout(dock_move, 780);
+}
+
+function dock_move() {
+    var dock = document.getElementsByTagName("div")[6];
+    if (tot1 == 1) {
+        dock.style = "margin-top: -600px; opacity: 100%;";
+    }
+    else if (tot1 >= 2) {
+        dock.style = "margin-top: 0px; opacity: 0%;";
         tot1 = 0;
     }
+    
 }
+
+function dock_search() {
+    if (event.keyCode == "13") {
+        if (document.getElementById('win-search').value != '') {
+            var app = document.getElementsByTagName("div")[9];
+            var App = document.getElementsByTagName("div")[12];
+            var str = document.getElementById('win-search').value;
+            if (str == '设置') {
+                app.style = "opacity: 0%";
+                App.style = "opacity: 100%; transform: translate(-250px, -258px);";
+                document.getElementsByTagName("div")[13].style = "opacity: 100%;";
+                document.getElementsByTagName("div")[14].style = "opacity: 0%;";
+                document.getElementsByTagName("div")[15].style = "opacity: 0%;";
+            }
+            else if (str == 'Microsoft Edge') {
+                app.style = "opacity: 0%";
+                App.style = "opacity: 100%; transform: translate(-250px, -258px);";
+                document.getElementsByTagName("div")[13].style = "opacity: 0%;";
+                document.getElementsByTagName("div")[14].style = "opacity: 100%; transform: translate(287px, -46px);";
+                document.getElementsByTagName("div")[15].style = "opacity: 0%;";
+            }
+            else {
+                app.style = "opacity: 0%";
+                App.style = "opacity: 100%; transform: translate(-250px, -258px);";
+                document.getElementsByTagName("div")[13].style = "opacity: 0%;";
+                document.getElementsByTagName("div")[14].style = "opacity: 0%;";
+                document.getElementsByTagName("div")[15].style = "opacity: 100%; transform: translate(0px, -70px);";
+            }
+        }
+        else {
+            document.getElementsByTagName("div")[9].style = "opacity: 100%;";
+            document.getElementsByTagName("div")[12].style = "opacity: 0%; transform: translate(-220px, -80px);";
+        }
+    }
+}
+
+function edge() {
+    document.getElementsByTagName("div")[19].style = "display: block";
+}
+
+function Edge() {
+    document.getElementsByTagName("div")[19].style = "display: none";
+}
+
+// 调试 Start
+Start(); // 测试
+// start(); // 正式
+// 调试 End
